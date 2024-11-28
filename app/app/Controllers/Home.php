11661f5ffff45c54a0e4845264da9e6a7c3af7f4
AllabouchAnas <allabouch2004@gmail.com>
2024-11-28 14:02:08 +0100

    fix admin dashboard stats

--- app/app/Controllers/Home.php
+++ app/app/Controllers/Home.php
@@ -2,6 +2,10 @@
 
 namespace App\Controllers;
 
+use App\Models\ProduitModel;
+use App\Models\CommandeModel;
+use App\Models\ClientModel;
+
 class Home extends BaseController
 {
     public function index(): string
@@ -9,8 +13,32 @@ class Home extends BaseController
         return view('index');
     }
 
-    public function admin(): string
+     public function admin(): string
     {
-        return view('admin/index');
+        $produitModel = new ProduitModel();
+        $commandeModel = new CommandeModel();
+        $clientsModel = new ClientModel();
+        
+        // Total Articles Vendus
+        $clientsInscrits = $clientsModel->countAll();
+
+        // Total Commandes
+        $totalCommandes = $commandeModel->countAllResults();
+
+        // Produits Totaux
+        $produitsTotaux = $produitModel->countAllResults();
+
+        // Valeur du Stock
+        $produits = $produitModel->findAll();
+        $valeurStock = array_reduce($produits, function($sum, $produit) {
+            return $sum + ($produit['quantite_en_stock'] * $produit['prix']);
+        }, 0);
+
+        return view('admin/index', [
+            'clientsInscrits' => $clientsInscrits,
+            'totalCommandes' => $totalCommandes,
+            'produitsTotaux' => $produitsTotaux,
+            'valeurStock' => $valeurStock,
+        ]);
     }
 }
