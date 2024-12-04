b6f264ba02635c07720bda23740196b25446c131
AllabouchAnas <allabouch2004@gmail.com>
2024-12-04 11:42:22 +0100

    update index view

--- app/app/Views/index.php
+++ app/app/Views/index.php
@@ -4,11 +4,15 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login</title>
+    <title>Redirecting...</title>
+    <script type="text/javascript">
+        // Redirect to the login page immediately when the page loads
+        window.location.href = "http://localhost:8080/login";
+    </script>
 </head>
 
 <body>
-    <a href="http://localhost:8080/admin">login</a>
+    <p>Redirecting to login...</p>
 </body>
 
 </html>
